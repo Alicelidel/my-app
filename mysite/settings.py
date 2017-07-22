a5d3cd3419f90a846ed78a90bd4253ddd4115048
Alicelidel <Alicelidel@users.noreply.github.com>
2017-07-22 23:09:31 +0300

    extends to blog pages

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '($-oes*bx2f*4!towxu2a1c$*!q@imxdo#h_zt0%e__3l3$w9l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
